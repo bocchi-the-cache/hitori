b5249293c95c993ec31b81f323864ffed66b98f1
sptuan <sptuan@steinslab.io>
2023-03-16 18:11:49 +0800

    refactor: config path

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestConfigInitWithPath(t *testing.T) {
-	Init("example/hitori.yaml")
+	Init("../../config/hitori.yaml")
 	t.Logf("Config: %+v", Cfg)
 	if Cfg.Server.Port != 8080 {
 		t.Errorf("Port not match")
